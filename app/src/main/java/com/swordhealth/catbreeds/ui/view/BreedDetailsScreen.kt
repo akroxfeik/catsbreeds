02b61cd6480dbe696f83dd156dd5a4658190e561
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 14:16:58 +0100

    create breed list and details pages

---
+++ app/src/main/java/com/swordhealth/catbreeds/ui/view/BreedDetailsScreen.kt
@@ -0,0 +1,8 @@
+package com.swordhealth.catbreeds.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun BreedDetailsScreen(navController: NavController) {
+}
