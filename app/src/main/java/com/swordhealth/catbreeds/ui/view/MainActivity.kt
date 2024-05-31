2b85035d2170343ea5a9ec6db597795adb6f828c
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 14:19:10 +0100

    create app entry point

---
+++ app/src/main/java/com/swordhealth/catbreeds/ui/view/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.swordhealth.catbreeds.ui.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.swordhealth.catbreeds.ui.AppNavHost
+import com.swordhealth.catbreeds.ui.theme.CatbreedsTheme
+import com.swordhealth.catbreeds.ui.viewmodel.BreedViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CatbreedsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavHost(navController = rememberNavController())
+                }
+            }
+        }
+    }
+}
