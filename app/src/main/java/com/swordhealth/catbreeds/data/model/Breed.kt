249d2978098688b8122c34122221921687e27596
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 14:17:15 +0100

    create data class breed

---
+++ app/src/main/java/com/swordhealth/catbreeds/data/model/Breed.kt
@@ -0,0 +1,29 @@
+package com.swordhealth.catbreeds.data.model
+
+import com.squareup.moshi.Json
+
+data class Breed (
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "temperament")
+    val temperament: String,
+    @Json(name = "origin")
+    val origin: String,
+    @Json(name = "country_codes")
+    val country_codes: String,
+    @Json(name = "country_code")
+    val country_code: String,
+    @Json(name = "life_span")
+    val life_span: String,
+    @Json(name = "wikipedia_url")
+    val wikipedia_url: String,
+    @Json(name = "weight")
+    val weight: Weight)
+
+data class Weight(
+    @Json(name = "imperial")
+    val imperial: String? = null,
+    @Json(name = "metric")
+    val metric: String? = null)
