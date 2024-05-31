d3e1b3a11c8b291b88a8e49047f43912d6899e78
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 14:19:55 +0100

    main repo strategy

---
+++ app/src/main/java/com/swordhealth/catbreeds/data/repository/MainRepository.kt
@@ -0,0 +1,8 @@
+package com.swordhealth.catbreeds.data.repository
+
+import com.swordhealth.catbreeds.data.api.ApiHelper
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(private val apiHelper: ApiHelper) {
+    suspend fun getBreeds() =  apiHelper.getBreeds()
+}
