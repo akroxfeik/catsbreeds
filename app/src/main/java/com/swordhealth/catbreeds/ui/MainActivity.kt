38c7562cddc0a93165d2e30e9b6adb0fbf339597
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 22:39:30 +0100

    restructuring EntryPoint

--- app/src/main/java/com/swordhealth/catbreeds/ui/MainActivity.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/MainActivity.kt
@@ -1,25 +1,14 @@
-package com.swordhealth.catbreeds.ui.view
+package com.swordhealth.catbreeds.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.swordhealth.catbreeds.ui.AppNavHost
 import com.swordhealth.catbreeds.ui.theme.CatbreedsTheme
-import com.swordhealth.catbreeds.ui.viewmodel.BreedViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
