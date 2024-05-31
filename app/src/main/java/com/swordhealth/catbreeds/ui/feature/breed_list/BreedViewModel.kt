d2955e413020b87b7d93a933e5fb5f22a3ee778e
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-05-31 22:42:39 +0100

    adapting view model for compose

---
+++ app/src/main/java/com/swordhealth/catbreeds/ui/feature/breed_list/BreedViewModel.kt
@@ -0,0 +1,52 @@
+package com.swordhealth.catbreeds.ui.feature.breed_list
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.swordhealth.catbreeds.data.model.Breed
+import com.swordhealth.catbreeds.data.repository.MainRepository
+import com.swordhealth.catbreeds.utils.NetworkHelper
+import com.swordhealth.catbreeds.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BreedViewModel @Inject constructor(
+    private val mainRepository: MainRepository,
+    private val networkHelper: NetworkHelper
+) : ViewModel() {
+
+    var state by mutableStateOf(
+        BreedContract.State(
+            breeds = listOf(),
+            isLoading = true
+        )
+    )
+        private set
+
+    var effects = Channel<BreedContract.Effect>(UNLIMITED)
+
+
+    init {
+        viewModelScope.launch { fetchBreeds() }
+    }
+
+    private fun fetchBreeds() {
+        viewModelScope.launch {
+            if (networkHelper.isNetworkConnected()) {
+                mainRepository.getBreeds().let {
+                    if (it.isSuccessful) {
+                        state = state.copy(breeds = it.body() ?: listOf(), isLoading = false)
+                    } else effects.send(BreedContract.Effect.Error)
+                }
+            } else effects.send(BreedContract.Effect.NoDataConnection)
+        }
+    }
+}
