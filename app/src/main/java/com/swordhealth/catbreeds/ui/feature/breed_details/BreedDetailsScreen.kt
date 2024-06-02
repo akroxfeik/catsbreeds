3487dabc4d4b5d762ab6e540b60d10f9ae923238
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-02 23:58:15 +0100

    add image to breed details

--- app/src/main/java/com/swordhealth/catbreeds/ui/feature/breed_details/BreedDetailsScreen.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/feature/breed_details/BreedDetailsScreen.kt
@@ -1,16 +1,21 @@
 package com.swordhealth.catbreeds.ui.feature.breed_details
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.swordhealth.catbreeds.data.model.Breed
 import kotlinx.coroutines.flow.Flow
 
@@ -29,8 +34,19 @@ fun BreedDetailsScreen(
 @Composable
 fun BreedDetails(breed: Breed?) {
     breed?.let {
-        Column(Modifier.padding(5.dp)){
-            Text(text = "Name: ${breed.name}")
+        Column(Modifier.padding(10.dp).fillMaxWidth()){
+            Text(
+                text = breed.name,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(5.dp))
+            Column(Modifier.fillMaxWidth()){
+                Image(
+                    painter = rememberImagePainter(
+                        data = "https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg"
+                    ),
+                    contentDescription = null,
+                )
+            }
             Text(text = "Origin: ${breed.origin}")
             Text(text = "Temperament: ${breed.temperament}")
             Text(text = "Description:\n${breed.description}")
