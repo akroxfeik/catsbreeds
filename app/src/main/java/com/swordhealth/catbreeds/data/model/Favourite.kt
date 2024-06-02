8bf2a523152456bf204a8dc1d3b2af4ce87872af
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-03 00:13:21 +0100

    add model favourite

---
+++ app/src/main/java/com/swordhealth/catbreeds/data/model/Favourite.kt
@@ -0,0 +1,28 @@
+package com.swordhealth.catbreeds.data.model
+
+import com.squareup.moshi.Json
+
+data class Favourite(
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "user_id")
+    val user_id: String,
+    @Json(name = "sub_id")
+    val sub_id: String,
+    @Json(name = "created_at")
+    val created_at: String
+)
+
+data class RequestFavourite(
+    @Json(name = "image_id")
+    val image_id: String,
+    @Json(name = "sub_id")
+    val sub_id: String
+)
+
+data class ResponseFavourite(
+    @Json(name = "message")
+    val message: String,
+    @Json(name = "id")
+    val id: String,
+)
