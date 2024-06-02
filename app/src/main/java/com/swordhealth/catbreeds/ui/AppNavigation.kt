00469628efe25e173edd13bec596019b229ddebb
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-02 23:50:12 +0100

    add favourites navigation option

--- app/src/main/java/com/swordhealth/catbreeds/ui/AppNavigation.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/AppNavigation.kt
@@ -1,12 +1,17 @@
 package com.swordhealth.catbreeds.ui
 
+object Arg {
+    const val BREED_ID = "breed_id"
+}
 
 enum class Screen {
     BREED_LIST,
-    BREED_DETAIL
+    BREED_DETAIL,
+    FAVOURITES
 }
 
 sealed class NavigationItem(val route: String) {
     object BreedList : NavigationItem(Screen.BREED_LIST.name)
     object BreedDetail : NavigationItem(Screen.BREED_DETAIL.name)
+    object Favourites : NavigationItem(Screen.FAVOURITES.name)
 }
