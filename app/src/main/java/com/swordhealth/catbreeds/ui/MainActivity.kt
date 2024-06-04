9c60da4c24cdb9b26a1a1e69c29e3824ed3dcbe7
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-04 01:04:58 +0100

    apply SnackbarAppState in MainActivity

--- app/src/main/java/com/swordhealth/catbreeds/ui/MainActivity.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.swordhealth.catbreeds.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.swordhealth.catbreeds.ui.feature.SnackbarAppState
+import com.swordhealth.catbreeds.ui.feature.rememberSnackbarAppState
 import com.swordhealth.catbreeds.ui.theme.CatbreedsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavHost(navController = rememberNavController())
+                    val appState: SnackbarAppState = rememberSnackbarAppState()
+                    Scaffold(
+                        snackbarHost = { SnackbarHost(appState.snackbarHostState) }
+                    ){
+                        AppNavHost(
+                            navController = rememberNavController(),
+                            showSnackbar = { message, duration ->
+                                appState.showSnackbar(message, duration)
+                            }
+                        )
+                    }
                 }
             }
         }
