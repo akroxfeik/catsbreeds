aa6dd95c0bd44a512f9cf35577a59236f588c209
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-04 02:49:20 +0100

    add avg life span to favs

--- app/src/main/java/com/swordhealth/catbreeds/ui/feature/favourites/FavouriteViewModel.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/feature/favourites/FavouriteViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.swordhealth.catbreeds.data.model.Breed
 import com.swordhealth.catbreeds.data.repository.MainRepository
 import com.swordhealth.catbreeds.utils.NetworkHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -41,7 +42,7 @@ class FavouriteViewModel @Inject constructor(
                         state.favourites.onEach { favourite ->
                             mainRepository.getBreed(favourite.image_id).let {
                                 if (it.isSuccessful) {
-                                    val breed = it.body()!!.copy(favourite = favourite)
+                                    val breed = it.body()!!.copy(favourite = favourite, name = title(it.body()!!))
                                     state = state.copy(breeds = state.breeds + breed, isLoading = false)
                                     effects.send(FavouriteContract.Effect.DataWasLoaded)
                                 } else error()
@@ -59,5 +60,18 @@ class FavouriteViewModel @Inject constructor(
     private suspend fun error() {
         state = state.copy(isLoading = false)
         effects.send(FavouriteContract.Effect.Error)
+    }
+
+    private fun title(breed: Breed): String {
+        return "${breed.name} (~ ${calculateAvgLifeSpan(breed.life_span)} years)"
+    }
+
+    private fun calculateAvgLifeSpan(life_span: String): String {
+        val lifeSpan = life_span.split(" - ")
+        return if (lifeSpan.size == 1) {
+            lifeSpan[0]
+        } else {
+            return "${((lifeSpan[0].toInt() + lifeSpan[1].toInt()) / 2)}"
+        }
     }
 }
