e83fd0318f3832e91d3c893167ebd4188205dfea
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2024-06-04 23:40:37 +0100

    customizing image loading + removing annoying messages

--- app/src/main/java/com/swordhealth/catbreeds/ui/feature/breed_details/BreedDetailsScreen.kt
+++ app/src/main/java/com/swordhealth/catbreeds/ui/feature/breed_details/BreedDetailsScreen.kt
@@ -1,22 +1,26 @@
 package com.swordhealth.catbreeds.ui.feature.breed_details
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.rememberImagePainter
+import coil.compose.SubcomposeAsyncImage
 import com.swordhealth.catbreeds.data.model.Breed
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
@@ -30,15 +34,16 @@ fun BreedDetailsScreen(
     LaunchedEffect(effectFlow) {
         effectFlow.onEach { effect ->
             when (effect) {
-                is BreedDetailsContract.Effect.DataWasLoaded -> {
+                /*is BreedDetailsContract.Effect.DataWasLoaded -> {
                     showSnackbar("Breed details are loaded.", SnackbarDuration.Short)
-                }
+                }*/
                 is BreedDetailsContract.Effect.Error -> {
                     showSnackbar("An error occurred.", SnackbarDuration.Short)
                 }
                 is BreedDetailsContract.Effect.NoDataConnection -> {
                     showSnackbar( "No data connection.", SnackbarDuration.Short)
                 }
+                else -> {}
             }
         }.collect()
     }
@@ -53,18 +58,21 @@ fun BreedDetails(viewModel: BreedDetailsViewModel, item: Breed?) {
         Column(
             Modifier
                 .padding(10.dp)
-                .fillMaxWidth()){
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ){
             Text(
                 text = item.name,
                 fontSize = 20.sp,
                 modifier = Modifier.padding(5.dp))
             Column(Modifier.fillMaxWidth()){
-                Image(
-                    painter = rememberImagePainter(
-                        // Bad approach to use hardcoded URL -> won't worry about it for this example
-                        data = "https://cdn2.thecatapi.com/images/${item.reference_image_id}.jpg"
-                    ),
-                    contentDescription = null,
+                SubcomposeAsyncImage(
+                    model = "https://cdn2.thecatapi.com/images/${item.reference_image_id}.jpg",
+                    contentDescription = "",
+                    loading = {
+                        CircularProgressIndicator(Modifier.size(25.dp))
+                    },
+                    contentScale = ContentScale.Crop,
                 )
             }
             Text(text = "Origin: ${item.origin}")
